9cf8ffd2589791d1639355af000400b9c05c314e
Paulo <pauljr235@gmail.com>
2024-10-21 13:52:05 +0100

    Construindo a estrutura e a seção 'Sobre mim'

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/pauljr235.png" alt="Foto perfil" />
+    <Titulo fontSize={20}>Paulo Lopes</Titulo>
+  </aside>
+)
+
+export default Sidebar
---
+++ src/containers/Sobre/index.tsx
@@ -0,0 +1,21 @@
+import Titulo from '../../components/Titulo'
+import Paragrafo from '../../components/Paragrafo'
+import { GithubSecao } from './styles'
+
+const Sobre = () => (
+  <section>
+    <Titulo fontSize={16}>Sobre mim</Titulo>
+    <Paragrafo tipo="secundario">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis sequi
+      deserunt fugiat magnam aliquid inventore, cumque dolorum possimus
+      architecto fugit incidunt obcaecati placeat laudantium tenetur veniam et
+      error. Tempore, tempora!
+    </Paragrafo>
+    <GithubSecao>
+      <img src="https://github-readme-stats.vercel.app/api?username=pauljr235&show_icons=true&theme=dracula&include_all_commits=true&count_private=true" />
+      <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=pauljr235&layout=compact&langs_count=7&theme=dracula" />
+    </GithubSecao>
+  </section>
+)
+
+export default Sobre
